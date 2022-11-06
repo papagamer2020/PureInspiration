84985ecea342eb7ef77a11d532b56a16a40d06e9
Nate Farver <farvercreek@gmail.com>
2022-11-06 10:17:39 -0500

    added login js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 // signup modal
 const signupButton = document.querySelector("#signup");
 const modalSignUp = document.querySelector("#modal-sign-up");
@@ -13,3 +15,40 @@ signupButton.addEventListener("click", () => {
 modalBgSignup.addEventListener("click", () => {
     modalSignUp.classList.remove("is-active");
 });
+
+//close signup modal when x is clicked
+modalCloseSignup.addEventListener("click",() => {
+    modalSignUp.classList.remove("is-active");
+});
+
+async function signupFormHandler(event){
+    event.preventDefault();
+    
+    const first_name = document.querySelector("#firstname-signup").value.trim();
+    const last_name = document.querySelector("#lastname-signup").value.trim();
+    const email = document.querySelector("#emial-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (first_name && last_name && emial && password) {
+        const resopnse = await fetch("/api/users", {
+            method: "post",
+            body: JSON.stringify({
+                first_name,
+                last_name:
+                email,
+                password,
+            }),
+            header: {"Content-Type": "application/json"},
+        });
+
+    if (response.ok) {
+        document.location.replace("/PureInspiration");
+    } else {
+        alert(response.statusText);
+    }
+    }
+}
+
+document
+    .querySelector("#signup-form")
+    .addEventListener("#submit", signupFormHandler);
