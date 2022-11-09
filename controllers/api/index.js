0388b018388babbbb8f8a843dd44c9bb6eed6269
Nate Farver <farvercreek@gmail.com>
2022-11-08 22:52:07 -0500

    started the frame work for the index api

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+//routes will be required in here
+const userRoutes = require("./userRoutes");
+
+//routes will be setup here
+router.use('/users', userRoutes);
+
+module.exports = router;
